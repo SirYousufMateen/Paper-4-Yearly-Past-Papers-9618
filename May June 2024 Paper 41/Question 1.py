47ec435eedfbeed17c5961ac489ec533989cd4fa
SirYousufMateen <132977128+SirYousufMateen@users.noreply.github.com>
2025-05-17 12:34:56 +0500

    Update Question 1.py

--- May June 2024 Paper 41/Question 1.py
+++ May June 2024 Paper 41/Question 1.py
@@ -1,51 +1,54 @@
-global DataStored #integer
-global NumberItems #Integer 20 items
+#Part a
+global DataStored
+global NumberItems
 
 NumberItems = 0
 DataStored = []
+
 def Initialise():
     global DataStored
     global NumberItems
-    Valid = False
-    while(Valid == False):
-        NumberItems = int(input("How many numbers will you enter?")) #loop until < 20
-        if NumberItems > 0 and NumberItems< 21:
-           Valid = True
-    for Count in range(0, NumberItems):
+    NumberItems = int(input("How many numbers will you enter?"))
+    while NumberItems <1 or NumberItems>20:
+        NumberItems = int(input("How many numbers will you enter?"))
+    for count in range(NumberItems):
         DataStored.append(int(input("Enter number")))
 
 
 Initialise()
 print(DataStored)
 
-def BubbleSort():
+def Bubble():
     global DataStored
     global NumberItems
-    for Count in range(0, NumberItems):
-        for Count2 in range(0, NumberItems-1):
-            if DataStored[Count2] > DataStored[Count]:
-                DataStored[Count2], DataStored[Count] = DataStored[Count], DataStored[Count2]
+    for count in range(NumberItems):
+        for index in range(NumberItems-1):
+            if DataStored[index]>DataStored[index+1]:
+                temp = DataStored[index]
+                DataStored[index] = DataStored[index+1]
+                DataStored[index+1] = temp
 
-BubbleSort()
+
+Bubble()
 print(DataStored)
-
-
 def BinarySearch(DataToFind):
-	global DataStored
-	global NumberItems
-	First = 0
-	Last = NumberItems
-	while (First <= Last):
-		MidValue = int((First + Last) / 2)
+    global DataStored
+    global NumberItems
+    found = False
+    low = 0
+    high = NumberItems
+    mid = (low + high) // 2
+    while low < high:
+        if DataToFind == DataStored[mid]:
+            return mid
+        elif DataToFind > DataStored[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+        mid = (low + high) // 2
+    return -1
 
-		if DataToFind == DataStored[MidValue]:
-			return MidValue
-		if DataToFind < DataStored[MidValue]:
-			Last = MidValue - 1
-		else:
-			First = MidValue + 1
+searchvalue = int(input("Enter a number to find"))
+print(BinarySearch(searchvalue))
 
-	return -1
 
-Search = int(input("Enter a number to find"))
-print(BinarySearch(Search))

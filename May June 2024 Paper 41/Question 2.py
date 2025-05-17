0c9c181d5c2a7f4ad215d0a083ff60d48d3164ad
SirYousufMateen <132977128+SirYousufMateen@users.noreply.github.com>
2025-05-17 14:37:15 +0500

    Update Question 2.py

--- May June 2024 Paper 41/Question 2.py
+++ May June 2024 Paper 41/Question 2.py
@@ -1,78 +1,76 @@
 class Tree:
-    def __init__(self, Name, HGrowth, MaxH, MaxW, PEvergreen):
-        self.__TreeName = Name
-        self.__HeightGrowth = HGrowth
-        self.__MaxHeight = MaxH
-        self.__MaxWidth = MaxW
-        self.__Evergreen = PEvergreen
+    #self.__Name = Name
+    #self.__HGrowth = HGrowth
+    #self.__MaxH = MaxH
+    #self.__MaxW = MaxW
+    #self.__EverGreen = EverGreen
+    def __init__(self,Name, HGrowth, MaxH, MaxW, EverGreen):
+        self.__Name = Name
+        self.__HGrowth = HGrowth
+        self.__MaxH=MaxH
+        self.__MaxW = MaxW
+        self.__EverGreen = EverGreen
 
-	def GetTreeName(self):
-		return self.__TreeName
-
-	def GetMaxHeight(self):
-		return self.__MaxHeight
-
-	def GetMaxWidth(self):
-		return self.__MaxWidth
-
-	def GetGrowth(self):
-		return self.__HeightGrowth
-
-	def GetEvergreen(self):
-		return self.__Evergreen
-
+    def GetTreeName(self):
+        return self.__Name
+    def  GetGrowth(self):
+        return self.__HGrowth
+    def  GetMaxHeight(self):
+        return self.__MaxH
+    def GetMaxWidth(self):
+        return self.__MaxW
+    def GetEvergreen(self):
+        return self.__EverGreen
 
 def ReadData():
-	TreeObjects = []
-	try:
-		File = open("Trees.txt")
-		TreeData = []
-		TreeData = File.read().split("\n")
-		SplitTrees = []
-		for Item in TreeData:
-			SplitTrees.append(Item.split(","))
-		File.close()
-		for Item in SplitTrees:
-			TreeObjects.append(Tree(Item[0], int(Item[1]), int(Item[2]), int(Item[3]), Item[4]))
-	except IOError:
-		print("invalid file")
-	return TreeObjects
+    TreeObject = []
+    try:
+        FileName = open("Trees.txt","r")
+        TreeData = []
+        TreeData = FileName.read().split("\n")
+        SplitTrees = []
+        for item in TreeData:
+            SplitTrees.append(item.split(","))
+        FileName.close()
+        for Item in SplitTrees:
+            TreeObject.append(Tree(Item[0], int(Item[1]), int(Item[2]), int(Item[3]), Item[4]))
+    except IOError:
+        print("File is not present")
+    return TreeObject
 
+def PrintTrees(item):
+    message = " It does not lose its leaves"
+    if item.GetEvergreen() == "No":
+        message = " It loses its leaves each year"
+    print(item.GetTreeName(),"has a maximum height",item.GetMaxHeight(),"a maximum width",item.GetMaxWidth(),"and grows",item.GetGrowth(),message)
 
-def PrintTrees(Item):
-	Final = "does not lose its leaves"
-	if Item.GetEvergreen() == "No":
-		Final = "loses its leaves each year"
-		print(Item.GetTreeName(), "has a maximum height", Item.GetMaxHeight(), "a maximumwidth",Item.GetMaxWidth()," and grows", Item.GetGrowth(),"cmayear.It",Final)
-
-
-TreeObjects = ReadData()
-PrintTrees(TreeObjects[0])
+TreeObjectArray = ReadData()
+PrintTrees(TreeObjectArray[2])
 
 def ChooseTree(Trees):
-    Evergreen = input("Do you want a tree that loses its leaves (enter lose), or keeps its leaves (enter keep)")
-    MaxHeight = int(input("What is the maximum tree height in cm"))
-    MaxWidth = int(input("What is the maximum tree width in cm"))
-    Options = []
-    if Evergreen.lower() == "keep" or Evergreen.lower() == "keep leaves" or Evergreen.lower() == "keeps its leaves":
+    MaxHeight= int(input("Enter maximum height"))
+    MaxWidth = int(input("Enter maximum width"))
+    Evergreen = input("Enter lose (if you want to loses its trees), Enter Keep(if you dont want to loses its trees")
+    option = []
+    if Evergreen.lower()  == "keep":
         keep = "Yes"
     else:
         keep = "No"
-    for Item in Trees:
-		if Item.GetMaxHeight() <= MaxHeight and Item.GetMaxWidth() <= MaxWidth and keep == Item.GetEvergreen():
-			Options.append(Item)
-			PrintTrees(Item)
-		if len(Options) == 0:
-			print("No suitable trees")
+    for item in Trees:
+        if item.GetMaxHeight() <=MaxHeight and item.GetMaxWidth()<=MaxWidth and keep == item.GetEvergreen():
+            option.append(item)
+            PrintTrees(item)
+        if len(option) == 0:
+            print("No suitable trees")
+    Valid = False
+    while Valid == False:
+        Choice = input("Enter the name of the tree you want")
+        for Item in option:
+            if Item.GetTreeName() == Choice:
+                Valid = True
+                Selected = Item
+                Start = int(input("Enter the height of the tree you would like to start with in cm"))
+                Years = (Selected.GetMaxHeight() - Start) / Selected.GetGrowth()
+                print("Your tree should be full height in approximately", Years, "years")
 
-
-Valid = False
-while Valid == False:
-   Choice = input("Enter the name of the tree you want")
-   for Item in Options:
-      if Item.GetTreeName() == Choice:
-         Valid = True
-         Selected = Item
-         Start = int(input("Enter the height of the tree you would like to start with in cm"))
-         Years =  (Selected.GetMaxHeight() - Start)/Selected.GetGrowth()
-         print("Your tree should be full height in approximately", Years,"years")
+ChooseTree(TreeObjectArray)
